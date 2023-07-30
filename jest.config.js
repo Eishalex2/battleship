42113f3bb93adbb6e88a2c2a977d90f6367eee69
Eishalex2 <114970998+Eishalex2@users.noreply.github.com>
2023-07-30 17:51:12 -0400

    Initial file setup
    Eslint, prettier, babel, jest, webpack

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  modulePaths: ['/shared/vendor/modules'],
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleDirectories: ['node_modules', 'bower_components', 'shared'],
+
+  moduleNameMapper: {
+    '^config$': '<rootDir>/configs/app-config.js',
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: ['node_modules'],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.gif$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: "./configs/app-config.js",
+      react: "./vendor/react-master",
+    },
+    extensions: [".js", ".jsx"],
+    modules: [
+      "node_modules",
+      "bower_components",
+      "shared",
+      "/shared/vendor/modules",
+    ],
+  },
+  "transform": {
+    "\\.[jt]sx?$": "babel-jest",
+    "\\.css$": "some-css-transformer",
+  },
+  modulePaths: ['/shared/vendor/modules'],
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleDirectories: ['node_modules', 'bower_components', 'shared'],
+
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+
+    '^react(.*)$': '<rootDir>/vendor/react-master$1',
+    '^config$': '<rootDir>/configs/app-config.js',
+  },
+};
